ebd90cfc6313dc2786ee9bbef76e7faf1efc985f
Ruslan <safiullinruslan717@gmail.com>
2026-07-05 15:58:19 +0300

    Создал папку фронтенд. Настроил подключение к бэкенду.

---
+++ frontend/src/api/api.js
@@ -0,0 +1,59 @@
+import axios from 'axios'
+
+
+const api = axios.create({
+    baseURL: 'http://127.0.0.1:8000/'
+}) 
+
+// Проверка есть ли токен
+api.interceptors.request.use(
+    (confirm) => {
+        const token = localStorage.getItem('access_token')
+        if (token) {
+            confirm.headers.Authorization = `Bearer ${token}`
+        }
+        return confirm
+    },
+    (error) => Promise.reject(error)
+)
+
+// автоматическая перезагрузка токена при ошибке 401
+api.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        originalRequest = error.config
+
+        if (error.response?.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true
+
+            try {
+                const refreshToken = localStorage.getItem('refresh_token')
+                if (!refreshToken) {
+                    throw new Error('Нет refresh-токена!')
+                }
+
+                const response = await axios.post('http://127.0.0.1:8000/api/token/refresh/', {
+                    refresh: refreshToken
+                })
+
+                const accessToken = (await response).data.accessToken
+
+                localStorage.setItem('access_token', accessToken)
+
+                originalRequest.headers.Authorization = `Bearer ${accessToken}`
+
+                return api(originalRequest)
+            } catch(err) {
+                localStorage.removeItem('access_token')
+                localStorage.removeItem('refresh_token')
+                return Promise.reject(err)
+            }
+        }
+
+        return Promise.reject(error)
+    }
+)
+
+export default api
+
+
