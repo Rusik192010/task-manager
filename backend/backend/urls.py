c42eef40c57d830c7b1f5b86a66183b96a67753b
Ruslan <safiullinruslan717@gmail.com>
2026-07-06 10:50:52 +0300

    Добавил Регистрацию на фронтенде. Подключил корс. Исправил ошибки с адресами токенов

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -10,7 +10,7 @@ router.register(r'tasks', TaskViewSet, basename='tasks')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls), name='task'),
-    path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/register/', RegisterViewSet.as_view(), name='register')
 ]
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,53 @@
+import { useEffect, useState } from "react"
+import Login from "./components/Login"
+import Register from "./components/Register"
+
 function App() {
-  
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const [showLogin, setShowLogin] = useState(false)
+
+  useEffect(() => {
+    const token = localStorage.getItem('access_token')
+    if (token) {
+      setIsAuthenticated(true)
+    }
+  }, [])
+
+  const handleLogin = () => {
+    setIsAuthenticated(true)
+  }
+
+  const handleLogout = () => {
+    setIsAuthenticated(false)
+    localStorage.removeItem('access_token')
+    localStorage.removeItem('refresh_token')
+  }
+
+  const onShowLogin = () => {
+    setShowLogin(true)
+  }
+
+  const onShowRegister = () => {
+    setShowLogin(false)
+  }
+
+  if (!isAuthenticated) {
+    return (
+      <div>
+        {!showLogin ? (
+            <Register onRegister={handleLogin} showLogin={onShowLogin} />
+          ) : (
+            <Login onLogin={handleLogin} showRegister={onShowRegister} />
+          )
+        }
+      </div>
+    )
+  }
 
   return (
     <>
       <h1>Менеджер задач</h1>
+      <button onClick={handleLogout}>Выйти</button>
     </>
   )
 }
